771d18157646a4d18cd8c87f99e28cbfd5c5897c
Maurice <maurice20schulz@gmail.com>
2026-05-20 10:13:58 +0200

    Initialize Spring Boot chess API foundation

---
+++ src/main/java/dev/maurice/chess/api/ChessEngineApiApplication.java
@@ -0,0 +1,13 @@
+package dev.maurice.chess.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessEngineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessEngineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/maurice/chess/api/ChessEngineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.maurice.chess.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessEngineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
